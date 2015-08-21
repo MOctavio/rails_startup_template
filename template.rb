8b3c91eb4ad4c323d878561fa3af9add509158b8
Mario Octavio Jimenez <jimenez303@gmail.com>
2015-08-21 13:15:27 -0600

    Ruby Sandbox Template
    Ready for Heroku and forked to match specific needs.

--- template.rb
+++ template.rb
@@ -1,38 +1,27 @@
-# Gems
-# ==================================================
-
-# Segment.io as an analytics solution (https://github.com/segmentio/analytics-ruby)
-gem "analytics-ruby"
+# Ruby version for Heroku
+ruby "2.2.0"
 # For encrypted password
 gem "bcrypt-ruby"
-# Useful SASS mixins (http://bourbon.io/)
-gem "bourbon"
-
 # For authorization (https://github.com/ryanb/cancan)
 gem "cancan"
-
-case ask("Choose Template Engine:", :limited_to => %w[erb haml slim])
-when "haml"
-  # HAML templating language (http://haml.info)
-  gem "haml-rails"
-when "slim"
-  # A lightweight templating engine (http://slim-lang.com)
-  gem "slim-rails"
-when "erb"
-end
-
+# CSS made fun! (http://sass-lang.com/)
+gem 'sass'
+# Useful SASS mixins (http://bourbon.io/)
+gem "bourbon"
+# HAML templating language (http://haml.info)
+gem "haml-rails"
+# Font-Awesome Sass gem (https://github.com/FortAwesome/font-awesome-sass)
+gem 'font-awesome-rails'
 # Simple form builder (https://github.com/plataformatec/simple_form)
 gem "simple_form", git: "https://github.com/plataformatec/simple_form"
-# To generate UUIDs, useful for various things
-gem "uuidtools"
-
+# Pg is the Ruby interface to the PostgreSQL RDBMS (http://www.postgresql.org/)
+gem 'pg'
 gem_group :development do
   # Rspec for tests (https://github.com/rspec/rspec-rails)
   gem "rspec-rails"
   # Guard for automatically launching your specs when files are modified. (https://github.com/guard/guard-rspec)
   gem "guard-rspec"
 end
-
 gem_group :test do
   gem "rspec-rails"
   # Capybara for integration testing (https://github.com/jnicklas/capybara)
@@ -43,78 +32,16 @@ gem_group :test do
   gem "factory_girl_rails"
   gem "database_cleaner"
 end
-
 gem_group :production do
   # For Rails 4 deployment on Heroku
   gem "rails_12factor"
 end
-
-
-# Setting up foreman to deal with environment variables and services
-# https://github.com/ddollar/foreman
-# ==================================================
-# Use Procfile for foreman
-run "echo 'web: bundle exec rails server -p $PORT' >> Procfile"
-run "echo PORT=3000 >> .env"
-run "echo '.env' >> .gitignore"
-# We need this with foreman to see log output immediately
-run "echo 'STDOUT.sync = true' >> config/environments/development.rb"
-
-
-
 # Initialize guard
 # ==================================================
 run "bundle exec guard init rspec"
-
-
-
 # Initialize CanCan
 # ==================================================
 run "rails g cancan:ability"
-
-
-
-# Clean up Assets
-# ==================================================
-# Use SASS extension for application.css
-run "mv app/assets/stylesheets/application.css app/assets/stylesheets/application.css.scss"
-# Remove the require_tree directives from the SASS and JavaScript files.
-# It's better design to import or require things manually.
-run "sed -i '' /require_tree/d app/assets/javascripts/application.js"
-run "sed -i '' /require_tree/d app/assets/stylesheets/application.css.scss"
-# Add bourbon to stylesheet file
-run "echo >> app/assets/stylesheets/application.css.scss"
-run "echo '@import \"bourbon\";' >>  app/assets/stylesheets/application.css.scss"
-
-
-
-
-# Bootstrap: install from https://github.com/twbs/bootstrap
-# Note: This is 3.0.0
-# ==================================================
-if yes?("Download bootstrap?")
-  run "wget https://github.com/twbs/bootstrap/archive/v3.0.0.zip -O bootstrap.zip -O bootstrap.zip"
-  run "unzip bootstrap.zip -d bootstrap && rm bootstrap.zip"
-  run "cp bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css vendor/assets/stylesheets/"
-  run "cp bootstrap/bootstrap-3.0.0/dist/js/bootstrap.js vendor/assets/javascripts/"
-  run "rm -rf bootstrap"
-  run "echo '@import \"bootstrap\";' >>  app/assets/stylesheets/application.css.scss"
-  run "rails g simple_form:install --bootstrap"
-end
-
-
-# Font-awesome: Install from http://fortawesome.github.io/Font-Awesome/
-# ==================================================
-if yes?("Download font-awesome?")
-  run "wget http://fontawesome.io/assets/font-awesome-4.1.0.zip -O font-awesome.zip"
-  run "unzip font-awesome.zip && rm font-awesome.zip && mv font-awesome-4.1.0 font-awesome"
-  run "cp font-awesome/css/font-awesome.css vendor/assets/stylesheets/"
-  run "cp -r font-awesome/fonts public/fonts"
-  run "rm -rf font-awesome"
-  run "echo '@import \"font-awesome\";' >>  app/assets/stylesheets/application.css.scss"
-end
-
-
 # Ignore rails doc files, Vim/Emacs swap files, .DS_Store, and more
 # ===================================================
 run "cat << EOF >> .gitignore
@@ -132,14 +59,11 @@ doc/
 .secret
 .DS_Store
 EOF"
-
-
 # Git: Initialize
 # ==================================================
 git :init
 git add: "."
 git commit: %Q{ -m 'Initial commit' }
-
 if yes?("Initialize GitHub repository?")
   git_uri = `git config remote.origin.url`.strip
   unless git_uri.size == 0
